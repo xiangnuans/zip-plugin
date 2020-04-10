02ae73ed04cd4728b38c36ce785dd90e4b22a1fd
guoyanhong <1352118502@qq.com>
2020-04-11 01:16:10 +0800

    init

---
+++ plugins/zip-plugin.js
@@ -0,0 +1,36 @@
+const JSZip = require('jszip');
+const { RawSource } = require('webpack-sources')
+const path = require('path')
+const zip  = new JSZip()
+
+module.exports = class ZipPlugin {
+  constructor(options) {
+    this.options = options
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('ZipPlugin', (compilation, callback) => {
+      const folder = zip.folder(this.options.filename);
+      for (let filename in compilation.assets) {
+        const source = compilation.assets[filename].source;
+        folder.file(filename, source);
+      }
+      zip.generateAsync({ type: 'nodebuffer' })
+      .then((content) => {
+        const outputPath = path.join(
+          compilation.options.output.path,
+          this.options.filename + '.zip');
+          const outputRelativePath = path.relative(
+            compilation.options.output.path,
+            outputPath
+          )
+        compilation.assets[outputRelativePath] = new RawSource(content)
+        callback()
+      })
+    })
+    // compiler.plugin('emit', (compilation, cb) => {
+    //   compilation.assets[name] = new RawSource('demo')
+    //   cb()
+    // })
+  }
+}
